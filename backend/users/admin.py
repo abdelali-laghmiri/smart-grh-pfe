08c1fc17d5afbed7cd1fd23e9bed61d8ad0db8ac
alighmiri <abdelalilaghmiri99@gmail.com>
2026-02-22 01:42:21 +0000

    finich all models in users app

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+# import all models
+from .models import Departement, JobPosition, Team, UserProfile, LeaveAdjustment
 # Register your models here.
+
+# Register Departement model
+admin.site.register(Departement)
+# Register JobPosition model
+admin.site.register(JobPosition)
+# Register Team model
+admin.site.register(Team)
+# Register UserProfile model
+admin.site.register(UserProfile)
+# Register LeaveAdjustment model
+admin.site.register(LeaveAdjustment)
+
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,6 +19,36 @@ class JobPosition(models.Model):
     def __str__(self):
         return f"{self.titre} (Level {self.level})"
 
-# 
+# class Team 
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    departement = models.ForeignKey(Departement, on_delete=models.CASCADE, related_name='teams')
+
+    def __str__(self):
+        return f"{self.name} ({self.departement})"
+    
+
+# class UserProfile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    job_position = models.ForeignKey(JobPosition, on_delete=models.SET_NULL, null=True, related_name='users')
+    departement = models.ForeignKey(Departement, on_delete=models.SET_NULL, null=True, related_name='users')
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
+    manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subordinates')
+    leave_balance = models.FloatField(default=0.0)
+    def __str__(self):
+        return self.user.username
+
+# class Leave Adjustment
+class LeaveAdjustment(models.Model):
+    employee = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='leave_adjustments')
+    ajusted_by = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, related_name='adjustments_made')
+    amount = models.FloatField()
+    reason = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.employee.user.username} | {self.amount}"
+    
 
     
