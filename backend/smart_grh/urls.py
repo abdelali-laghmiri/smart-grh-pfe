73ae42d6408c95fa220fb29471e8b444d9b69476
alighmiri <abdelalilaghmiri99@gmail.com>
2026-02-22 17:21:48 +0000

    static files

--- backend/smart_grh/urls.py
+++ backend/smart_grh/urls.py
@@ -23,3 +23,9 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/requests/', include('requests_engine.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf import settings
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
