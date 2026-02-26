d6976750dcdc8fa176d42e92c5aaaab78deb24cf
alighmiri <abdelalilaghmiri99@gmail.com>
2026-02-26 22:00:06 +0000

    creating aut shemas

---
+++ backend/apps/auth/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool
+    created_at: datetime
+    class Config:
+        from_attributes = True
