0ebf81150929e00fc2455ab82f9c14a31994deb3
alighmiri <abdelalilaghmiri99@gmail.com>
2026-02-26 21:03:27 +0000

    add megrations to project

---
+++ backend/apps/auth/services.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+# import the User and UserRole models
+from .models import User,UserRole
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password)-> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_user(
+        db : Session,
+        matricule: str,
+        password: str,
+        role: UserRole = UserRole.USER
+        ):
+    hashed_password = get_password_hash(password)
+    db_user = User(
+        matricule=matricule,
+        hashed_password=hashed_password,
+        role=role,
+        is_active=True,
+        first_login=True
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, matricule: str, password: str):
+    db_user = db.query(User).filter(User.matricule == matricule).first()
+    if not db_user:
+        return None
+    if not verify_password(password, db_user.hashed_password):
+        return None
+    return db_user
